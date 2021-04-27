3760a1d1b526c8c6d492fb0a7e1607fe7b665f23
Chicomangusta <andreptorres@gmail.com>
2021-04-27 18:34:06 -0400

    new NAME commit

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          ANDRE TORRES <a href="">DEVELOPER</a>
+          MANG TORRES <a href="">DEVELOPER</a>
         </h1>
 
         <p className={styles.description}>
